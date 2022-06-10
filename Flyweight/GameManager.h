e469e90b24d8332c8b576fab2c3d256917a0b8c9
alfiinnn <84430772+alfiinnn@users.noreply.github.com>
2022-06-10 13:58:44 +0700

    Add files via upload

---
+++ Flyweight/GameManager.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <stdlib.h>
+
+#include "ObjectFactory.h"
+
+class GameManager
+{
+	std::string typeArray[3] = { "circle", "triangle", "rectangle" };
+
+public:
+	std::string randType();
+	void randSize(Object*);
+	void randCoord(Object*);
+};
---
+++ Flyweight/ObjectFactory.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <map>
+#include "Object.h"
+#include "Rectangle.h"
+#include "Circle.h"
+#include "Triangle.h"
+
+class ObjectFactory
+{
+	std::map<std::string, Object*> objectMap;
+
+public:
+	Object* getObject(std::string);
+	void dispHash();
+};
